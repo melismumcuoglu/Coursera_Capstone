f3af47f7066371028bc6e98e16431c7c8db439ba
Melis Mumcuoğlu <02484311@ksnet.local>
2020-04-08 16:05:55 +0300

    project1

---
+++ Coursera_Capstone_Project_Week1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Coursera Capstone Project
+
+# *This notebook will be mainly used for the capstone project*
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[5]:
+
+
+print('Hello Capstone Project Course!')
+
